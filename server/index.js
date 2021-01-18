23c79a89c14a7003d89a4b6222b99e9a4f431dfc
Michael Egan <egan16@gmail.com>
2021-01-18 11:12:12 +0000

    Set up DB connection and connect to server app

--- server/index.js
+++ server/index.js
@@ -11,3 +11,17 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 //setting up cors
 app.use(cors());
+
+//MongoDB Atlas connection
+const CONNECTION_URL = 'mongodb+srv://mikeTBT:secret123@cluster0.9hada.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// PORT for running application
+
+//To connect DB to application
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    //above returns a promise, need a .then with arrow function for a successful connection
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    //.catch with arrow function for an unsuccessful connection
+    .catch((error) => console.log(error.message));
+
+//takes care of potential warnings in console 
+mongoose.set('useFindAndModify', false);
