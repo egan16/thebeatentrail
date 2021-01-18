44e8a697115bf0a8a3634318c227934d520d73f8
Michael Egan <egan16@gmail.com>
2021-01-18 16:23:53 +0000

    Tidy up project

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 //connecting to the div with the id root
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+    );
