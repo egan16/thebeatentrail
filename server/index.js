ae538819003e5f1d0b5582e1f2b84e614ae83720
Michael Egan <egan16@gmail.com>
2021-01-18 10:33:47 +0000

    Server index.js set up app, bodyParser and cors

--- server/index.js
+++ server/index.js
@@ -3,3 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+//initialise this application
+const app = express();
+
+//setting up body parser to send successful requests
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+//setting up cors
+app.use(cors());
