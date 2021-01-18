371d1e394d54dadacad38d34d4150512a156bac9
Michael Egan <egan16@gmail.com>
2021-01-18 13:08:46 +0000

    Add create trip router

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 //imports functions from /controllers/trips.js
-import { getTrips } from '../controllers/trips.js'
+import { getTrips , createTrip} from '../controllers/trips.js'
 
 const router = express.Router();
 
 //app.use('/trips', tripRoutes); in index.js changes this to http://localhost:5000/trips
 router.get('/', getTrips);
+router.post('/', createTrip);
 
 export default router;
