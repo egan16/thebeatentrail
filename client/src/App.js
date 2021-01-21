ab3c82138ea336318aac31de474d8ce5e6f788b3
Michael Egan <egan16@gmail.com>
2021-01-21 14:06:16 +0000

    Update App.js to class (extends component)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Component } from 'react';
 import { Container } from 'react-bootstrap';
 
-const App = () => {
+import './css/App.css';
+
+class App extends Component {
+  // For storing state
+  constructor(props) {
+    // call super constructor in parent class (Component class)
+    super(props);
+    // bind property called this.state - ("this." refers to object created from the class template)
+    this.state = {
+        
+    };
+  }
+
+  render() {
     return (
-        <Container>
-            Test
-        </Container>
+      <Container>
+          Test
+        {/* <Navigation/> */}
+      </Container>
     );
+  }
 }
 
 export default App;
