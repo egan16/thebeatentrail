3f000eff4d51801671f6d5146e1da306518b948e
Michael Egan <egan16@gmail.com>
2021-01-25 14:41:31 +0000

    Return action.payload for FETCH_ALL

--- client/src/reducers/trips.js
+++ client/src/reducers/trips.js
@@ -4,7 +4,7 @@ export default (trips = [], action) => {
     //returns state in switch statements depending on the action
     switch (action.type) {
         case 'FETCH_ALL':
-            return trips;
+            return action.payload; //action.payload is the trips retrieved from actions
         case 'CREATE':
             return trips;
         default:
