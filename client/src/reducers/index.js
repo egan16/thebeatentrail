a0249e8a7e885f750a3ab83f13254a0604071e2d
Michael Egan <egan16@gmail.com>
2021-01-25 12:53:12 +0000

    Export combined reducers in reducers/index.js

---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from redux;
+
+import trips from './trips';
+
+//inside here include all individual reducers for application
+export default combineReducers({
+    trips,
+});
