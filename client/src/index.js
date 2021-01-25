984c1ea2bbab5e03e3814b8024c8fb6691037fd3
Michael Egan <egan16@gmail.com>
2021-01-25 12:46:07 +0000

    Initialise redux in  index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/index.css';
 import App from './App';
+
+
+//for redux
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 //connecting to the div with the id root
 ReactDOM.render(
