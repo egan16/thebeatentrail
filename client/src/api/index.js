e3a3e69998d792adde9788432d91ee54508b7e2e
Michael Egan <egan16@gmail.com>
2021-02-08 15:36:49 +0000

    export function to delete trip by id in client>api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,3 +6,5 @@ const url = 'http://localhost:5000/trips';
 //EXPORTING API REQUESTS
 export const fetchTrips = () => axios.get(url); //exported function to get trips from backend on another file
 export const createTrip = (newTrip) => axios.post(url, newTrip); //exported function to create trip
+//do edit here
+export const deleteTrip = (id) => axios.delete(`${url}/${id}`); //exported function to delete trip by id
