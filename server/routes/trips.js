993d1e1c39d126571ed2ec141dbd0b525fccff99
Michael Egan <egan16@gmail.com>
2021-02-08 15:15:00 +0000

    Add delete trip route to server

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 //imports functions from /controllers/trips.js
-import { getTrips , createTrip} from '../controllers/trips.js'
+import { getTrips , createTrip, deleteTrip} from '../controllers/trips.js'
 
 const router = express.Router();
 
 //app.use('/trips', tripRoutes); in index.js changes this to http://localhost:5000/trips
 router.get('/', getTrips);
 router.post('/', createTrip);
+//edit route here
+router.delete('/:id', deleteTrip);
 
 export default router;
