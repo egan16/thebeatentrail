45510354197bd448398df2f58534013e6d50467c
Michael Egan <egan16@gmail.com>
2021-02-15 21:15:30 +0000

    Display nav links depending on user state

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -4,10 +4,10 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
 const Navigation = () => {
-  // const user = null;
+  const user = null;
   return (
       <Navbar bg="light" expand="lg" className="mb-5">
-        <Navbar.Brand href="#home">
+        <Navbar.Brand as={Link} to='/'>
             <img
                 src="logo.svg"
                 width="30"
@@ -18,37 +18,34 @@ const Navigation = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav.Item className="ml-auto">
-            <Nav.Link>Login</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>Register</Nav.Link>
-          </Nav.Item>
           <Nav.Item>
             <Nav.Link as={Link} to='/'>Home</Nav.Link>
           </Nav.Item>
+          {user === null &&
           <Nav.Item className="ml-auto">
-            <Nav.Link>Logout</Nav.Link>
-          </Nav.Item>
-          
+            <Nav.Link as={Link} to='/auth'>Login</Nav.Link>
+          </Nav.Item>}
+          {user === null &&
+          <Nav.Item>
+            <Nav.Link>Register</Nav.Link>
+          </Nav.Item>}
+          {user !== null &&
             <Navbar.Brand href="#home">
                 <img
                     src="logo.svg"
                     width="30"
                     height="30"
                     className="d-inline-block align-top"
-                    alt="The Beaten Trail"
+                    alt="user name"
                 />
-            </Navbar.Brand>
-            <Nav.Item>
+            </Navbar.Brand>}
+            {user !== null &&
+          <Nav.Item>
+            <Nav.Link>Logout</Nav.Link>
+          </Nav.Item>}
+          <Nav.Item>
               {/* <Nav.Link><Image alt={user.result.name} src={user.result.imageUrl} roundedCircle>{user.result.name.charAt(0)}</Image></Nav.Link> */}
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>Register</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>Register</Nav.Link>
-            </Nav.Item>
+          </Nav.Item>
         </Navbar.Collapse>
       </Navbar>
   )
