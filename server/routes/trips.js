3979d78c06e0b1cd6b2850516a30c75b5322cc83
Michael Egan <egan16@gmail.com>
2021-02-15 13:29:39 +0000

    Add update route

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -1,14 +1,14 @@
 import express from 'express';
 
 //imports functions from /controllers/trips.js
-import { getTrips , createTrip, deleteTrip, likeTrip} from '../controllers/trips.js'
+import { getTrips , createTrip, updateTrip, deleteTrip, likeTrip} from '../controllers/trips.js'
 
 const router = express.Router();
 
 //app.use('/trips', tripRoutes); in index.js changes this to http://localhost:5000/trips
 router.get('/', getTrips);
 router.post('/', createTrip);
-// router.patch('/:id', updateTrip);   //DONT FORGET TO IMPORT updateTrip up top
+router.patch('/:id', updateTrip);
 router.delete('/:id', deleteTrip);
 router.patch('/:id/likeTrip', likeTrip);
 
