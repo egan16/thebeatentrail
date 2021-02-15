05518ccbf64365792a2ee58e75a21c863d117343
Michael Egan <egan16@gmail.com>
2021-02-15 12:15:40 +0000

    Add likeTrip route

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -1,14 +1,15 @@
 import express from 'express';
 
 //imports functions from /controllers/trips.js
-import { getTrips , createTrip, deleteTrip} from '../controllers/trips.js'
+import { getTrips , createTrip, deleteTrip, likeTrip} from '../controllers/trips.js'
 
 const router = express.Router();
 
 //app.use('/trips', tripRoutes); in index.js changes this to http://localhost:5000/trips
 router.get('/', getTrips);
 router.post('/', createTrip);
-//edit route here
+// router.patch('/:id', updateTrip);   //DONT FORGET TO IMPORT updateTrip up top
 router.delete('/:id', deleteTrip);
+router.patch('/:id/likeTrip', likeTrip);
 
 export default router;
