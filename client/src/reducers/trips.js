f2b2856f32e744f53ce8099fb45c31b6e29cb168
Michael Egan <egan16@gmail.com>
2021-02-15 12:56:36 +0000

    Add LIKE reducer

--- client/src/reducers/trips.js
+++ client/src/reducers/trips.js
@@ -4,7 +4,9 @@ export default (trips = [ ], action) => {
     switch (action.type) {
         case 'DELETE':
             return trips.filter((trip) => trip._id != action.payload) //return all the trips except to the trip where id is equal to action.payload
-        //edit goes here
+        // case 'UPDATE':   //only remove comment will work as is!!
+        case 'LIKE':
+            return trips.map((trip) => trip._id == action.payload._id ? action.payload : trip); //map() over the trips, check what was the trip that was liked using id and then if trip was liked return the trip with the change, else return the trip as it was.
         case 'FETCH_ALL':
             return action.payload; //action.payload is the trips retrieved from actions
         case 'CREATE':
