54f4fc29d004f828397d537bf70d5f9a9e4eb1c0
Michael Egan <egan16@gmail.com>
2021-02-15 14:18:16 +0000

    Add updateTrip api call

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,6 +6,6 @@ const url = 'http://localhost:5000/trips';
 //EXPORTING API REQUESTS
 export const fetchTrips = () => axios.get(url); //exported function to get trips from backend on another file
 export const createTrip = (newTrip) => axios.post(url, newTrip); //exported function to create trip
-// export const updateTrip = (id, updatedTrip) => axios.patch(`${url}/${id}`, updatedTrip); //exported function to update a trip by id
+export const updateTrip = (id, updatedTrip) => axios.patch(`${url}/${id}`, updatedTrip); //exported function to update a trip by id
 export const deleteTrip = (id) => axios.delete(`${url}/${id}`); //exported function to delete trip by id
 export const likeTrip = (id) => axios.patch(`${url}/${id}/likeTrip`); //exported function to update likes on a trip by id
