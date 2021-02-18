efc64fce32bb7cf1cbe3edc033d7a8affe29aa33
Michael Egan <egan16@gmail.com>
2021-02-18 23:04:11 +0000

    logout reducer and dispatch logout action

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,24 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { useDispatch } from 'react-redux';
 
 const Navigation = () => {
 
   //set user state from localstorage
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
 
-  console.log(user);
+  const dispatch = useDispatch();  
+  const history = useHistory();
+  const location = useLocation();
+
+  //logout fuction to call logout action
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' });
+    //push to home when user logs out
+    history.push('/');
+    //set user = null
+    setUser(null);
+  };
 
   //automatically re-navigate 
   useEffect(() => {
     //if token exists send to token variable
-    const token = user?.token;
+    // const token = user?.token; //THIS COMMMENTED OUT BECAUSE CAUSING ERROR
 
     //manual sign up token (JWS) here when done
 
+    // when location changes set the user
     setUser(JSON.parse(localStorage.getItem('profile')));
-  }, []);
+  }, [location]);
 
   return (
       <Navbar bg="light" expand="lg" className="mb-5">
@@ -41,18 +54,23 @@ const Navigation = () => {
             <Nav.Link as={Link} to='/auth'>Sign In</Nav.Link>
           </Nav.Item>}
           {user !== null &&
-            <Navbar.Brand href="#home">
+            <Navbar.Brand href="#home" className="ml-auto">
                 <img
-                    src="logo.svg"
+                    src={user?.result.imageUrl}
                     width="30"
                     height="30"
                     className="d-inline-block align-top"
-                    alt="user name"
+                    alt={user?.result.name.charAt(0)}
                 />
             </Navbar.Brand>}
+          {user !== null && 
+            <Navbar.Text>
+              <a href="#login">{user?.result.name}</a>
+            </Navbar.Text>
+          }
             {user !== null &&
           <Nav.Item>
-            <Nav.Link>Logout</Nav.Link>
+            <Nav.Link onClick={logout}>Logout</Nav.Link>
           </Nav.Item>}
           <Nav.Item>
               {/* <Nav.Link><Image alt={user.result.name} src={user.result.imageUrl} roundedCircle>{user.result.name.charAt(0)}</Image></Nav.Link> */}
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,7 +5,11 @@ const authReducer = (state = { authData: null }, action) => {
             localStorage.setItem('profile', JSON.stringify({ ... action?.data }));
             //set login data to authData state
             return { ... state, authData: action?.data };
-    
+        case 'LOGOUT':
+            //clear localstorage when user logs out
+            localStorage.clear();
+            //set authData state to null
+            return { ... state, authData: null };
         default:
             return state;
     }
