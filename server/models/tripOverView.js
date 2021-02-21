f1696ecac43bd21cca5df518cdb9a532e0c553c1
Michael Egan <egan16@gmail.com>
2021-02-21 22:24:03 +0000

    Remove likeCount and add likes array to tripSchema

--- server/models/tripOverView.js
+++ server/models/tripOverView.js
@@ -7,9 +7,9 @@ const tripSchema = mongoose.Schema({
     user: String,
     tags: [String],
     selectedFile: String,
-    likeCount: {
-        type: Number,
-        default: 0
+    likes: {
+        type: [String],
+        default: []
     },
     createdAt: {
         type: Date,
