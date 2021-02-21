c0b92e6eb0c21ddb92afbefbbecbbe5aeb9940ec
Michael Egan <egan16@gmail.com>
2021-02-21 21:47:28 +0000

    Create auth middleware

---
+++ server/controllers/user.js
@@ -0,0 +1,73 @@
+//bcrypt used to hash the password for security
+import bcrypt from 'bcryptjs';
+//to store the user in a browser for some time as a token
+import jwt from 'jsonwebtoken';
+
+//import user model
+import User from '../models/user.js';
+
+//secret string for token
+const secret = 'test';
+
+//signin controller
+export const signin = async (req, res) => {
+    //get email and password from the frontend post request available in req.body
+    const { email, password } = req.body;
+
+    //try/catch because function is async
+    try {
+        //if sigining in, need to find the user (by email)
+        const existingUser = await User.findOne({ email });
+
+        //if there is no user with the email, return 404 error
+        if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
+
+        //need to check if the password matches the hashed password of the existing user
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+
+        //if password incorrect return 400 error
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+        //if email and password match successfully, get the users token
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, secret, { expiresIn: "1h" });
+
+        //now have the token, need to return it with the user
+        res.status(200).json({ result: existingUser, token });
+    } catch (error) {
+        //if token creation was not successful
+        res.status(500).json({ message: "Something went wrong with token" });
+    }
+}
+
+//signup controller
+export const signup = async (req, res) => {
+    //get email, password, confirmPassword, firstName, lastName from the frontend post request available in req.body
+    const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+    //try/catch because function is async
+    try {
+        //if sigining up, need to find the user (by email) in the db
+        const existingUser = await User.findOne({ email });
+
+        //if there a user with the email, return 400 error
+        if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+        //if the passwords do not match, return 400 error
+        if(password != confirmPassword) return res.status(400).json({ message: "Passwords don't match" });
+
+        //hash password for security - (2nd param) salt is level of difficulty used to hash the password
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        //if no existing user and passwords match, a new user can be created
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+
+        //if user created successfully, create the users token
+        const token = jwt.sign({ email: result.email, id: result._id }, secret, { expiresIn: "1h" });
+
+        //now have the token, need to return it with the new user
+        res.status(200).json({ result: result, token });
+    } catch (error) {
+        //if token creation was not successful
+        res.status(500).json({ message: "Something went wrong with token" });
+    }
+}
---
+++ server/middleware/auth.js
@@ -0,0 +1,38 @@
+import jwt, { decode } from 'jsonwebtoken';
+
+//example of middleware if successful
+//user clicks like button => auth middleware (NEXT) => like controller...
+
+const secret = 'test';
+
+const auth = async (req, res, next) => {
+    //to check is the users token valid
+    try {
+        //get token from the frontend (token is in the first position in the array after its split)
+        const token = req.headers.authorization.split(" ")[1];
+        //is token the applications token or googles Oauth token (apps token is less then 500)
+        const isCustomAuth = token.length < 500;
+
+        //variable created for data gotten from the token itself
+        let decodedData;
+
+        //if applications token is valid gets users id
+        if(token && isCustomAuth) {
+            //data (username and id) of each specific token of logged in user
+            decodedData = jwt.verify(token, secret);
+            //store logged in users id in req.userId
+            req.userId = decodedData?.id;
+        } else {
+            //if googles Oauth token is valid gets users id
+            decodedData = jwt.decode(token);
+            //store logged in users id in req.userId (sub is googles version of id for every google user)
+            req.userId = decodedData?.sub;
+        }
+        //if auth middleware successful pass action onto the next task
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default auth;
