2178517fa6ee208883492bd4d00ec76e293112bf
Michael Egan <egan16@gmail.com>
2021-02-22 12:09:44 +0000

    Modify trip api request endpoints

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios'; //used to make api calls
 
 //URL on backend route
-const url = 'http://localhost:5000/trips';
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 //EXPORTING API REQUESTS
-export const fetchTrips = () => axios.get(url); //exported function to get trips from backend on another file
-export const createTrip = (newTrip) => axios.post(url, newTrip); //exported function to create trip
-export const updateTrip = (id, updatedTrip) => axios.patch(`${url}/${id}`, updatedTrip); //exported function to update a trip by id
-export const deleteTrip = (id) => axios.delete(`${url}/${id}`); //exported function to delete trip by id
-export const likeTrip = (id) => axios.patch(`${url}/${id}/likeTrip`); //exported function to update likes on a trip by id
+export const fetchTrips = () => API.get('/trips'); //exported function to get trips from backend on another file
+export const createTrip = (newTrip) => API.post('/trips', newTrip); //exported function to create trip
+export const updateTrip = (id, updatedTrip) => API.patch(`/trips/${id}`, updatedTrip); //exported function to update a trip by id
+export const deleteTrip = (id) => API.delete(`/trips/${id}`); //exported function to delete trip by id
+export const likeTrip = (id) => API.patch(`/trips/${id}/likeTrip`); //exported function to update likes on a trip by id
