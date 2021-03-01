76e08569f32feb2c75cc540a0e3dbb3c3226ab8a
Michael Egan <egan16@gmail.com>
2021-03-01 11:36:26 +0000

    create places reducer

---
+++ client/src/reducers/places.js
@@ -0,0 +1,10 @@
+//Reducer function accepts state and action
+export default (places = [ ], action) => {
+    //returns state in switch statements depending on the action
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return action.payload; //action.payload is the places retrieved from actions
+        default:
+            return places;
+    }
+}
