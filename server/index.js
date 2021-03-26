78565fd2567a9824e99b83ca4c623324a7a315aa
Michael Egan <egan16@gmail.com>
2021-03-26 15:20:05 +0000

    gitignore

--- server/index.js
+++ server/index.js
@@ -82,7 +82,7 @@ PythonShell.runString('x=1+1;print(x)', null, function (err, result) {
 //MongoDB Atlas connection
 const CONNECTION_URL = process.env.DB_HOST;
 // PORT for running application
-// const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 //test
 
 //To connect DB to application
